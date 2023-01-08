3fa8821678093a8badd3ed5125f48cc64e2a7a16
eesh <eesh1996@gmail.com>
2023-01-08 21:30:02 +0530

    minor changes

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -17,7 +17,7 @@ const generateAction = async (req, res) => {
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.8,
+    temperature: 0.7,
     max_tokens: 1200,
   });
 
